70bdae2c300bf80f669366e2ffc5001b9d3c0a22
Baptiste PECATTE <baptpecatte@gmail.com>
2017-01-04 18:18:43 +0100

    Moved notif server addresses to a setting file, and removed the default one

--- data-server/push_app/notif_server_handler.py
+++ data-server/push_app/notif_server_handler.py
@@ -1,8 +1,5 @@
 from push_app.notif_server_client import NotifServerClient
-
-NOTIF_SERVERS_ADDR = [
-    ("127.0.0.1", 8081)
-]
+import settings
 
 class NotifServerHandler:
     
@@ -19,7 +16,7 @@ class NotifServerHandler:
     def __init__(self):
         self.notif_servers = []
         
-        for a in NOTIF_SERVERS_ADDR:
+        for a in settings.NOTIF_SERVERS_ADDR:
             client = NotifServerClient(self, a[0], a[1])
             self.notif_servers.append(client)
             
