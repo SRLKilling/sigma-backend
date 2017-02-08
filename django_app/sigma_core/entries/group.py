314b5efb133040a0e04461ef622aaff073438d63
Nutemm <matton.alex@gmail.com>
2017-02-08 15:00:23 +0100

    ameliore les entries/rempli group_invitation

--- django_app/sigma_core/entries/group.py
+++ django_app/sigma_core/entries/group.py
@@ -4,26 +4,19 @@ from sigma_api.importer import load_ressource
 Group = load_ressource("Group")
 
 class GroupEntrySet(entries.EntrySet):
-    
+
     list = entries.list(
         Group.objects.user_can_see,
         Group.serializers.list
     )
-    
-    retrieve = entries.retrieve(
-        Group.objects,
-        Group.serializers.default
-    )
-    
-    create = entries.create(
-        Group.serializers.default
-    )
-    
+
+    retrieve = entries.retrieve()
+
+    create = entries.create()
+
     # update_right = entries.update(
         # Group.serializers.default,
         # 'update_right'
     # )
-    
-    destroy = entries.destroy(
-        Group.objects
-    )
+
+    destroy = entries.destroy()
