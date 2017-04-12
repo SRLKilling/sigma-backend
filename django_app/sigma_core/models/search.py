1c9072f3a2ff0adf355a1dad4e58fe15e33a4548
fandejuni <t.dardinier@gmail.com>
2017-04-12 16:07:25 +0200

    Added distance edition

--- django_app/sigma_core/models/search.py
+++ django_app/sigma_core/models/search.py
@@ -12,10 +12,35 @@ class Search(models.Model):
         fantom
     """
     objects = SearchQuerySet.as_manager()
-    
+
     #*********************************************************************************************#
     #**                                    Permissions                                          **#
     #*********************************************************************************************#
+
+    def levenshtein_distance(a, b):
+        n = len(a)
+        m = len(b)
+
+        c_effacement = 1
+        c_insertion = 1
+        c_substitution = 1
+
+        d = [[0 for k in range(m + 1)] for k in range(n + 1)]
+        for i in range(n + 1):
+            d[i][0] = i
+        for j in range(m + 1):
+            d[0][j] = j
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                c_substitution_local = c_substitution
+                if a[i-1] == b[j-1]:
+                    c_substitution_local = 0
+                r = d[i-1][j] + c_effacement
+                r = min(r, d[i][j-1] + c_insertion)
+                r = min(r, d[i-1][j-1] + c_substitution_local)
+                d[i][j] = r
+        return d[n][m]
 
     def can_print_list(self, user):
         return True
